705e2ace873b5477cb2475f8f734ab17f5dc70be
Moudi <mjaderi97@gmail.com>
2023-11-28 11:50:31 +1100

    Created dummy login to test, tweaked tailwind config & package.json files

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+//Loads the handlebars module
+const handlebars = require('express-handlebars');
+const exp = require('constants');
+
+//Sets our app to use the handlebars engine
+app.set('view engine', 'hbs');
+
+//Sets handlebars configurations (we will go through them later on)
+app.engine('hbs', handlebars({
+  layoutsDir: path.join(__dirname, '/views/layouts'),
+  partialsDir: path.join(__dirname, '/views/partials'),
+  extname: 'hbs',
+}));
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('homepage', { layout: 'index' });
+});
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
