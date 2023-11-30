652cac736656168c2272aefa3a9b2a4b80ab43dd
Hybee <hybee234@outlook.com>
2023-11-30 22:58:36 +1100

    API routes for Brand set up

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const brandRoutes = require('./brandRoutes')
+
 router.use('/users', userRoutes);
 router.use('/brand', brandRoutes);
+
 module.exports = router;
 
 
