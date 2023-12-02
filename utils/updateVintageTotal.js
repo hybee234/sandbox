71d3172a457f9585967d46cfb4485652e673f1c8
Hybee <hybee234@outlook.com>
2023-12-02 20:45:04 +1100

    Updates to updateVintageTotal Utility

--- utils/updateVintageTotal.js
+++ utils/updateVintageTotal.js
@@ -4,14 +4,12 @@ const { Vintage, Transaction } = require('../models');
 const updateVintageTotal = async (vintage_id) => {
 
     // Find SUM of qty_in for the Vintage ID of interest (active transactions only)
-    const totalQtyIn = await Transaction.sum('qty_in',           
-        // {where: { vintage_id: req.params.vintage_id , active_ind: 1}}
+    const totalQtyIn = await Transaction.sum('qty_in',  
         {where: { vintage_id: vintage_id , active_ind: 1}}
     );
     
     // Find SUM of qty_out for the Vintage ID of interest (active transactions only)
-    const totalQtyOut = await Transaction.sum('qty_out',           
-        // {where: { vintage_id: req.params.vintage_id, active_ind: 1 }}
+    const totalQtyOut = await Transaction.sum('qty_out',   
         {where: { vintage_id: vintage_id, active_ind: 1 }}
     );
 
@@ -22,13 +20,12 @@ const updateVintageTotal = async (vintage_id) => {
         console.log (`\x1b[32m Updated Vintage Total: \x1b[0m${vintageTotal}`)
 
     // Write vintage total to vintage table
-    const updateVintageTotal = await Vintage.update (
+    await Vintage.update (
         {
             vintage_total: vintageTotal
         },
         {
             where: {
-                // vintage_id: req.params.vintage_id
                 vintage_id: vintage_id
             }
         }
